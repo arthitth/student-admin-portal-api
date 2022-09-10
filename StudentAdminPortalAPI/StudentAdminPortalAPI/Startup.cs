973502e369cc5aa3c95c0b969075bcfde299c018
arthitth <the.arthit@gmail.com>
2022-09-10 21:44:53 +0700

    End of Section 9

--- StudentAdminPortalAPI/StudentAdminPortalAPI/Startup.cs
+++ StudentAdminPortalAPI/StudentAdminPortalAPI/Startup.cs
@@ -1,3 +1,4 @@
+
 using Microsoft.AspNetCore.Builder;
 using Microsoft.AspNetCore.Hosting;
 using Microsoft.AspNetCore.HttpsPolicy;
@@ -16,6 +17,7 @@ using System.Collections.Generic;
 using System.IO;
 using System.Linq;
 using System.Threading.Tasks;
+using FluentValidation.AspNetCore;
 
 namespace StudentAdminPortalAPI
 {
@@ -43,6 +45,9 @@ namespace StudentAdminPortalAPI
             });
 
             services.AddControllers();
+
+            services.AddFluentValidation(fv => fv.RegisterValidatorsFromAssemblyContaining<Startup>());
+
             services.AddDbContext<StudentAdminContext>(options =>
                options.UseSqlServer(Configuration.GetConnectionString("StudentAdminPortalDb")));
       
